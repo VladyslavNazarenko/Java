b47b71244e0644f06716cb4d8dfda11f9f22142a
VladyslavNazarenko <153461651+VladyslavNazarenko@users.noreply.github.com>
2024-09-08 21:55:23 +0300

    B01_02.java

--- B01_02.java
+++ B01_02.java
@@ -2,7 +2,9 @@ package Hw1;
 
 public class B01_02 {
 	
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        for (int i = args.length - 1; i >= 0; i--) {
+            System.out.print(args[i] + " ");
+        }
+    }
 }
