e1e8084f45848235162c99cf69250c50e3207d8f
VladyslavNazarenko <153461651+VladyslavNazarenko@users.noreply.github.com>
2024-09-08 21:56:07 +0300

    B01_03.java

--- B01_03.java
+++ B01_03.java
@@ -1,8 +1,19 @@
 package Hw1;
 
 public class B01_03 {
-	
-	public static void main(String[] args) {
-		
-	}
+
+    public static void main(String[] args) {
+        double d = 1.0;
+
+        for (int j = 0; j < args.length; j++) {
+            System.out.println(args[j]);
+            try {
+                d *= Double.parseDouble(args[j]);
+            } catch (NumberFormatException e) {
+                System.err.println("Bad argument!");
+                return;
+            }
+        }
+        System.out.printf("Добуток: %.4f", d);
+    }
 }
